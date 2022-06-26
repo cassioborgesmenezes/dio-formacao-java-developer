5d166558675d93565c29f70e24369fe3ec0e7824
Cassio MENEZES <cassio.menezes@outlook.com.br>
2022-06-26 11:17:25 -0300

    feat: criar classe tipos

---
+++ java-anatomia-classe/src/tech/cbm/anatomia/core/TiposVariaveis.java
@@ -0,0 +1,9 @@
+package tech.cbm.anatomia.core;
+
+public class TiposVariaveis {
+	double salarioMinio = 2500D;
+
+	short numeroCurto = 1;
+	int numeroNormal = numeroCurto;
+	short numeroCurtoNovo = numeroNormal;
+}
